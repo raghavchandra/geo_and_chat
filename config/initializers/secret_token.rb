df0d569952328fcda9bbc50e9b36300ac70196b0
Raghav Chandra-Home <raghavchandra-home@tw-mbp13-rchandra.local>
2012-11-28 01:38:54 -0800

    checkin_app: INITIAL SETUP.  rails new checkin_app. using rmvrc 1.9.3@checkin_app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheckinApp::Application.config.secret_token = 'a091fa346d293fcff0eeb333ff8f124cb4e38cac721a875a13052c5eecf71746090cb984edda31253ce8794f08baeda8acb15790f8372e901f56d475dba956a6'
